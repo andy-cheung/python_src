8744fba0c8e9e514676a0500902665752b05c5ad
andy.cheung <andy@andycheungtekiMacBook-Pro.local>
2013-07-27 23:43:53 +0800

    =change space to tab

--- checkfile.py
+++ checkfile.py
@@ -2,30 +2,30 @@ import os,sys
 from hashlib import md5
 
 def calMD5(str):
-    m = md5()
-    m.update(str)
-    return m.hexdigest() 
+	m = md5()
+	m.update(str)
+	return m.hexdigest() 
 def calMD5ForFile(file):
-    m = md5()
-    a_file = open(file, 'rb')
-    m.update(a_file.read())
-    a_file.close()
-    return m.hexdigest()
+	m = md5()
+	a_file = open(file, 'rb')
+	m.update(a_file.read())
+	a_file.close()
+	return m.hexdigest()
 
 def check(rootdir, checkfile):
-    f = open(checkfile, 'r')
-    list = f.readlines() 
-    for line in list:
-        arg = line.split(",")
-        arg[1] = arg[1].replace('\n','')
-        cf = rootdir + arg[1]
-        str = calMD5ForFile(cf)
-        if str != arg[0]:
-            print(arg[1] + ":" + str + "|" + arg[0])
-            return False
-    return True
+	f = open(checkfile, 'r')
+	list = f.readlines() 
+	for line in list:
+		arg = line.split(",")
+		arg[1] = arg[1].replace('\n','')
+		cf = rootdir + arg[1]
+		str = calMD5ForFile(cf)
+		if str != arg[0]:
+			print(arg[1] + ":" + str + "|" + arg[0])
+			return False
+	return True
 succ = check(sys.argv[1], "listdir.txt") 
 if succ :
-    print("check ok")
+	print("check ok")
 else:
-    print("check fail")
+	print("check fail")
--- listfile.py
+++ listfile.py
@@ -2,31 +2,31 @@ import os,sys
 from hashlib import md5
 
 def calMD5(str):
-    m = md5()
-    m.update(str)
-    return m.hexdigest() 
+	m = md5()
+	m.update(str)
+	return m.hexdigest() 
 def calMD5ForFile(file):
-    m = md5()
-    a_file = open(file, 'rb')
-    m.update(a_file.read())
-    a_file.close()
-    return m.hexdigest()
+	m = md5()
+	a_file = open(file, 'rb')
+	m.update(a_file.read())
+	a_file.close()
+	return m.hexdigest()
 
 def listdir(dir,file,rootdir):
-    # file.write(dir + '\n')
-    fielnum = 0
-    # print(dir)
-    list = os.listdir(dir) 
-    for line in list:
-        filepath = os.path.join(dir,line)
-        # print(filepath)
-        if os.path.isdir(filepath): 
-            fielnum = fielnum + listdir(filepath, myfile, rootdir + "/" + line)
-        elif os.path: 
-            myfile.write(calMD5ForFile(filepath)  + ',' +  rootdir + "/" + line + '\n')
-            # print(filepath)
-            fielnum = fielnum + 1
-    return fielnum
+	# file.write(dir + '\n')
+	fielnum = 0
+	# print(dir)
+	list = os.listdir(dir) 
+	for line in list:
+		filepath = os.path.join(dir,line)
+		# print(filepath)
+		if os.path.isdir(filepath): 
+			fielnum = fielnum + listdir(filepath, myfile, rootdir + "/" + line)
+		elif os.path:
+			myfile.write(calMD5ForFile(filepath)  + ',' +  rootdir + "/" + line + '\n')
+			# print(filepath)
+			fielnum = fielnum + 1
+	return fielnum
 # dir = raw_input('please input the path:')
 myfile = open('listdir.txt','w')
 fielnum = listdir(sys.argv[1], myfile, "")
