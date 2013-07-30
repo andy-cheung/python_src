5bd02487e73ac7989b1c56fbd2bc928b6cc422e2
andy.cheung <andy@andycheungtekiMacBook-Pro.local>
2013-07-30 15:02:45 +0800

    =

--- execcmd.py
+++ execcmd.py
@@ -1,17 +1,24 @@
-import MySQLdb
-import os
+# import MySQLdb
+import string
+import os, re
 import sys
 import time
 import urllib
 import commands
 
+dbhost = "127.0.0.1"
+dbpwaswd = "123456"
+dbport = 3306
+dbname = "actor"
+
 def startSrv():
+	print("startSrv....")
 	try:
 		pid = os.fork()
 		if pid > 0:
 			sys.exit(0)
 	except Exception, e:
-		sys.stderr.write("fork 1 fail -->%d --> %s\n" % (e.errno, e.strerror))
+		sys.stderr.write("fork 1 fail")
 		sys.exit(1)
 	os.chdir("/")
 	os.setsid()
@@ -22,40 +29,77 @@ def startSrv():
 		if pid > 0:
 			sys.exit(0)
 	except Exception, e:
-		sys.stderr.write("fork 2 fail -->%d --> %s\n" % (e.errno, e.strerror))
+		sys.stderr.write("fork 2 fail")
 		sys.exit(1)
 
 	sys.stdout.flush()
 	sys.stderr.flush()
 
 def process(sid, cmdid, cmd, para1, para2):
+	print("recv:" + cmd)
+	output = ""
 	if cmd == "update":
 		status, output = commands.getstatusoutput("update.sh")
 		#upate:download and unzip
 		print(output)
-		if output == "update ok":
-			ret = "ok"
-		else:
-			ret = "fail"
 
 	#send result to web server
-	f = urllib.urlopen("http://localhost/test.jsp?cmdid="+cmdid+"&ret="+ret+"&sid="+sid)
+	f = urllib.urlopen("http://localhost/servercmd_callback.jsp?cmdid=%d&ret=%s&sid=%d" % (cmdid, output, sid))
 	s = f.read()
 	print("web result:", s)
 	f.close()
 
 if __name__ == '__main__':
+	dir = os.getcwd()
 	startSrv()
 	print "========================="
 	print("startSrv success!")
 	print "========================="
+	
+	os.chdir(dir)
+	
+	# load config
+	f = open("DBServer.txt", "r")
+	list = f.readlines() 
+	find = False
+	for line in list:
+		if line.find("SQL") >= 0:
+			find = True
+			continue
+		if find:
+			if line.find("Host") >= 0:
+				m = re.search(r"\"[^\"]*\"", line)
+				if m:
+					dbhost = m.group(0).replace("\"", "")
+					print dbhost
+				else:
+					print "error!!" + line
+					exit(-1)
+			if line.find("Port") >= 0:
+				m = re.search(r"\d+", line)
+				if m:
+					dbport = m.group(0)
+					print dbport
+				else:
+					print "error!!" + line
+					exit(-1)
+			if line.find("DBName") >= 0:
+				m = re.search(r"\"[^\"]*\"", line)
+				if m:
+					dbname = m.group(0).replace("\"", "")
+					print dbname
+				else:
+					print "error!!" + line
+					exit(-1)
 
 	#connect to mysql
-	conn = MySQLdb.Connect(host='localhost',user='root', pwaswd='', port=3306)
-	cur = conn.curosr()
+	conn = MySQLdb.Connect(host=dbhost, user='root', passwd="hoolai12", port=string.atoi(dbport), db=dbname)
+	print("connect to mysql succ!")
+
+	cur = conn.cursor()
 	while True:
 		# load cmd from db per 5 sec
-		cur.execute("select id,sid,cmdid, cmd, para1, para2 from servercmd limit 1")
+		cur.execute("select id,serverid,cmdid, cmd, param1, param2 from servercmd limit 1")
 		if cur.rowcount >= 1:
 			row = cur.fetchone()
 			id = row[0]
@@ -63,10 +107,8 @@ if __name__ == '__main__':
 			process(sid, row[2], row[3], row[4], row[5])
 						
 			#after process cmd, should delete from db
-			n = cur.execute("delete servercmd where id="+id)
+			n = cur.execute("delete from servercmd where id=%d" % id)
 			if n <= 0:
 				exit(-1)	#delete error,exit system
 
 		time.sleep(5)
-
-
